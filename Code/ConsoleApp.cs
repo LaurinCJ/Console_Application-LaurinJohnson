b2067c4150926d213e34567e8c9779fbb3320127
Laurin Johnson <laurincjohnson1@gmail.com>
2026-09-18 22:44:04 -0400

    Completely messed up setting up project, should be fixed and my program can build

---
+++ Code/ConsoleApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Application
+{
+    public class ConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testing");
+        }
+    }
+}
---
+++ Code/DataManager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Console_Application
+{
+    public class DataManager
+    {
+        
+    }
+}
