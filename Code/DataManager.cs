221074a8e8e084334182174a442cbfc81f2de8ba
Laurin Johnson <laurincjohnson1@gmail.com>
2026-09-19 00:37:13 -0400

    Fetch Data working

--- Code/DataManager.cs
+++ Code/DataManager.cs
@@ -17,7 +17,103 @@ namespace Console_Application
 
         public void FetchData() //Case 1: Get Data
         {
-            Console.WriteLine("Fetch Data successfully called");
+            String sql = "";
+            Console.WriteLine("Which table would you like to access? Enter the corresponding number.");
+            Console.WriteLine("Options: \n" +
+                              "1. Franchises \n" +
+                              "2. Titles \n");
+
+            String choiceInput = Console.ReadLine();
+
+            bool inputCheck = int.TryParse(choiceInput, out int choice);
+
+            while (inputCheck == false || (choice != 1 && choice != 2))
+            {
+                Console.WriteLine("\nPlease input a valid number.");
+                choiceInput = Console.ReadLine();
+
+                inputCheck = int.TryParse(choiceInput, out choice);
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    sql = "SELECT FranchiseID, Franchise FROM dbo.Franchises;";
+
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(ConnectionString))
+                        {
+                            connection.Open();
+
+                            SqlCommand command = new SqlCommand(sql, connection);
+
+                            Console.WriteLine("Successfully connected.");
+
+                            SqlDataReader reader = command.ExecuteReader();
+
+                            while (reader.Read())
+                            {
+                                var franchiseId = reader["FranchiseID"];
+                                var franchise = reader["Franchise"];
+
+                                Console.WriteLine($"{franchiseId}: {franchise}");
+                            }
+                        }
+                        Console.WriteLine("\nClosed Connection.");
+                    }
+
+                    catch (SqlException)
+                    {
+                        Console.WriteLine("Connection Failed. Returning to options.");
+                        return;
+                    }
+
+                    break;
+
+                case 2:
+                    sql = "SELECT t.GameID, t.GameTitle, t.Console, f.Franchise FROM dbo.Titles AS t INNER JOIN dbo.Franchises AS f ON t.FranchiseID = f.FranchiseID;"; //I did have ChatGPT generate this because I wasn't sure what else to do
+
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(ConnectionString))
+                        {
+                            connection.Open();
+
+                            SqlCommand command = new SqlCommand(sql, connection);
+
+                            Console.WriteLine("Successfully connected.");
+
+                            SqlDataReader reader = command.ExecuteReader();
+
+                            while (reader.Read())
+                            {
+                                var gameID = reader["GameID"];
+                                var gameTitle = reader["GameTitle"];
+                                var franchise = reader["franchise"];
+                                var console = reader["Console"];
+
+                                Console.WriteLine($"{gameID}: {gameTitle} - {console} - Franchise: {franchise}");
+                            }
+                        }
+                        Console.WriteLine("\nClosed Connection.");
+                    }
+
+                    catch (SqlException)
+                    {
+                        Console.WriteLine("Connection Failed. Returning to options.");
+                        return;
+                    }
+
+                    break;
+            }
+
+            
+        }
+
+        public void AddData() //Case 2: Add Data
+        {
+            Console.WriteLine("Add Data successfully called");
 
             try
             {
@@ -25,7 +121,10 @@ namespace Console_Application
                 {
                     connection.Open();
                     Console.WriteLine("Successfully connected.");
+
+
                 }
+                Console.WriteLine("\nClosed Connection.");
             }
 
             catch (SqlException)
@@ -33,23 +132,52 @@ namespace Console_Application
                 Console.WriteLine("Connection Failed. Returning to options.");
                 return;
             }
-
-            Console.WriteLine("Closed Connection.");
-        }
-
-        public void AddData() //Case 2: Add Data
-        {
-            Console.WriteLine("Add Data successfully called");
         }
 
         public void UpdateData() //Case 3: Update Data
         {
             Console.WriteLine("Update Data successfully called");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Successfully connected.");
+
+
+                }
+                Console.WriteLine("\nClosed Connection.");
+            }
+
+            catch (SqlException)
+            {
+                Console.WriteLine("Connection Failed. Returning to options.");
+                return;
+            }
         }
         
         public void DeleteData() //Case 4: Delete Data
         {
             Console.WriteLine("Delete Data successfully called");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Successfully connected.");
+
+
+                }
+                Console.WriteLine("\nClosed Connection.");
+            }
+
+            catch (SqlException)
+            {
+                Console.WriteLine("Connection Failed. Returning to options.");
+                return;
+            }
         }
     }
 }
--- Code/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ Code/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2067c4150926d213e34567e8c9779fbb3320127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9d2ec8dcde58e75139f1b138df1ca332be4d23")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
